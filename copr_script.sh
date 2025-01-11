c2084ea011882c0cadb6e32018aaf4c78c408169
Root <175176948+RKNF404@users.noreply.github.com>
2025-01-11 17:44:28 -0500

    chore: use new directories (#20)

--- copr_script.sh
+++ copr_script.sh
@@ -34,12 +34,11 @@ readonly LIST_SOURCES=(
     "https://raw.githubusercontent.com/lassekongo83/Frellwits-filter-lists/master/Frellwits-Swedish-Filter.txt"
     "https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_7_Japanese/filter.txt"
 )
-readonly NAME="hardened-chromium-subresource-filter"
+readonly NAME="trivalent-subresource-filter"
 
 # Clone the repo with the spec file and chrowmium source downloader
 cp $NAME/$NAME.spec ./
 cp $NAME/install_filter.sh ./
-cp $NAME/chromium-latest.py ./
 cp /usr/src/chromium/chromium-*-clean.tar.xz ./
 rm -rf ./$NAME
 
